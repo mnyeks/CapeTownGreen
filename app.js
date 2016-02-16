fb1a0a7939584bc4595f171c55e80a697f26a3db
mnyeks <0719075643kennyg@gmail.com>
2016-02-16 16:07:50 +0200

    first function

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
 
 //find the body tag and store it in a variable called 'body'
+/*var body = document.querySelector("body");
+
+//listen for the keydown event
+body.onkeydown = function(e){
+    //e.keyCode - will capture the key codes
+    //displayMessage(e.keyCode);
+};*/
+
+var keyCodeName = function(keycode) {
+	if(keycode === 39) {
+       return "right";
+   }
+
+   if(keycode === 37) {
+      return "left";
+   }
+
+   if(keycode === 38) {
+     return "up";
+   }
+
+   if(keycode === 40) {
+      return "down";
+   }
+
+   else {
+      return "";
+   }
+	};
+
+	//find the body tag and store it in a variable called 'body'
 var body = document.querySelector("body");
 
 //listen for the keydown event
@@ -7,3 +38,4 @@ body.onkeydown = function(e){
     //e.keyCode - will capture the key codes
     //displayMessage(e.keyCode);
 };
+	
