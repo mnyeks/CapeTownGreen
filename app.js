ad440bf5024ef293cfc457bc4d317bc2cf1aa44c
mnyeks <0719075643kennyg@gmail.com>
2016-02-18 16:34:44 +0200

    done

--- app.js
+++ app.js
@@ -19,11 +19,12 @@ var keyName = keyCodeName(e.keyCode);
      displayMessage(taxiLocationCounter);
 };
 
-var taxiLocationCounter = 1;
- var i =  0; 
-  function outcome() {
-     i = i++;
-  } 
+var taxiLocationCounter = 0;
+    taxiLocationCounter++;
+ //var i =  0; 
+ // function outcome() {
+    // i = i++;
+  //} 
 
 var keyCodeName = function(keyCode) {
 
@@ -48,9 +49,39 @@ var keyCodeName = function(keyCode) {
    }
 	};
 
-function createLocactionClass(number) {
-    if(number === 1) {
+function createLocationClass(taxiLocationCounter) {
+    if(taxiLocationCounter === 1) {
         return "slot-one-of-nine";
     }
-  };
+    if(taxiLocationCounter === 2) {
+        return "slot-two-of-nine";
+    }
+    if(taxiLocationCounter === 3) {
+        return "slot-three-of-nine";
+    }
+    if(taxiLocationCounter === 4) {
+        return "slot-four-of-nine";
+    }
+    if(taxiLocationCounter === 5) {
+        return "slot-five-of-nine";
+    }
+    if(taxiLocationCounter === 6) {
+        return "slot-six-of-nine";
+    }
+    if(taxiLocationCounter === 7) {
+        return "slot-seven-of-nine";
+    }
+    if(taxiLocationCounter === 8) {
+        return "slot-eight-of-nine";
+    }
+    if(taxiLocationCounter === 9) {
+        return "slot-nine-of-nine";
+    }
+ };
+
+ function moveForward() {
+  var currentLocation = createLocationClass(taxiLocationCounter);
+  
+
+ }
   
